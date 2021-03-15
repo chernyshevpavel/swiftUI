09c6f99178013c160246393ac9ba94075b3dacf9
Pavel Chernyshev <titan100695@yandex.ru>
2021-03-15 23:11:59 +0300

    menu screen

---
+++ toggle/toggle/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  toggle
+//
+//  Created by Павел Чернышев on 15.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var isOn = false
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: nil, content: {
+            ZStack(content: {
+                VStack(content: {
+                    HStack(content: {
+                        VStack(content: {
+                            Text("menu 1")
+                            Text("menu 2")
+                            Text("menu 3")
+                        }).font(.custom("", size: 25, relativeTo: .body))
+                        Spacer()
+                    })
+                    Spacer()
+                }).padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 0))
+                ZStack(content: {
+                    Rectangle().fill(Color.gray)
+                    Text("Hello, world!").fontWeight(.bold)
+                }).offset(x: isOn ? UIScreen.main.bounds.width * 0.35 : 0)
+                
+            })
+            Toggle(LocalizedStringKey.init("Show menu"), isOn: $isOn).padding()
+        }).animation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.3))
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ toggle/toggle/toggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  toggleApp.swift
+//  toggle
+//
+//  Created by Павел Чернышев on 15.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct toggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
