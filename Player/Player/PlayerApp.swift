35c1ebc23825448f072f9e931498058df1b90826
Pavel Chernyshev <titan100695@yandex.ru>
2021-03-17 23:23:15 +0300

    Player initial

---
+++ Player/Player/PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerApp.swift
+//  Player
+//
+//  Created by Павел Чернышев on 17.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
